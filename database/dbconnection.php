71a516c733f348dd968c131dbab61b1b2607a5ca
lastnato-bit <166599689+artdna1@users.noreply.github.com>
2025-04-10 19:13:12 +0800

    arrt

--- database/dbconnection.php
+++ database/dbconnection.php
@@ -1,78 +1,40 @@
 <?php
 
-
-class Database
-{
-    private $host;
-    private $db_name;
-    private $username;
-    private $password;
-    public $conn;
-
-
-    public function __construct()
+    class Database
     {
+        private $host;
+        private $db_name;
+        private $username;
+        private $password;
+        public $conn;
 
-
-        if($_SERVER['SERVER_NAME'] === 'localhost' || $_SERVER['SERVER_ADDR'] === '127.0.0.1' || $_SERVER['SERVER_ADDR'] === '192.168.1.72')
+        public function __construct()
         {
-
-
-            $this->host = "localhost";
-            $this->db_name = "itelec2";
-            $this->username = "root";
-            $this->password = "";
-
-
+            if($_SERVER['SERVER_NAME'] === 'localhost' || $_SERVER['SERVER_ADDR'] === '127.0.0.1' || $_SERVER['SERVER_NAME'] === '192.168.1.72'){
+                $this->host = "localhost";
+                $this->db_name = "itelec2";
+                $this->username = "root";
+                $this->password = "";
+            }
+            else{
+                $this->host = "localhost";
+                $this->db_name = "";
+                $this->username = "";
+                $this->password = "";
+            }
         }
 
-
-        else
+        public function dbConnection()
         {
-
-
-            $this->host = "localhost";
-            $this->db_name = "";
-            $this->username = "";
-            $this->password = "";
-
-
+            $this->conn = null;
+            try {
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $exception) 
+            {
+                echo "Connection error: " . $exception->getMessage();
+            }
+            return $this->conn;
         }
-
-
     }
-
-
-    public function dbConnection()
-    {
-
-
-        $this->conn = null;
-        try
-        {
-
-
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-        } catch (PDOException $exception)
-        {
-
-
-            echo "Connection error:" . $exception->getMessage();
-
-
-        }
-
-
-        return $this->conn;
-
-
-    }
-
-
-}
-
-
 ?>
